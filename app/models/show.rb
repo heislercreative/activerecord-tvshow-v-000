e5a167e1147bcd7a1980464cfb223d6d08b36594
Brennan Heisler <learn@heislercreative.com>
2018-03-12 03:10:10 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,7 +7,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    self.find_by(self.highest_rating)
+    self.find_by(rating: highest_rating)
   end
 
   def self.lowest_rating
